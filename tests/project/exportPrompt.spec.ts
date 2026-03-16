60d4a6d848c99bade12c71ab5223cb3f6e410ef6
singhkrishnakar <singh.krishnakar@gmail.com>
2026-03-16 19:45:58 +0530

    added tfixed spec file

--- tests/project/exportPrompt.spec.ts
+++ tests/project/exportPrompt.spec.ts
@@ -1,12 +1,28 @@
 import { test } from "../../framework/fixtures/projectDetail.fixture";
-import { ExportPromptOrchestrator } from "../../framework/orchestrators/exportPromptOrchestrator";
-import { PromptTestData } from "../../types/testData.type"
-import { promptData } from "../../data/prompts/promptData"
+import { ExportPromptsOrchestrator } from "../../framework/orchestrators/exportFilteredPromptsOrchestrator";
+import { promptData } from "../../data/prompts/promptData";
 
-test.only("Export Prompt", async ({ testContext }) => {
+test.describe("Export Prompts Tests", () => {
 
-    const orchestrator = new ExportPromptOrchestrator(testContext);
+    test("Export Prompt without filter", async ({ testContext }) => {
+        const orchestrator = new ExportPromptsOrchestrator(testContext);
+        await orchestrator.run(promptData[0]);
+    });
 
-    await orchestrator.run(promptData[0]);
+    test.only("Export Prompt with date filter", async ({ testContext }) => {
 
+        const orchestrator = new ExportPromptsOrchestrator(testContext);
+
+        const today = new Date();
+        const startDate = new Date(today.getTime() - 24 * 60 * 60 * 1000)
+            .toISOString()
+            .split("T")[0];
+
+        const endDate = today.toISOString().split("T")[0];
+
+        await orchestrator.run(promptData[0], {
+            filterByDateRange: { startDate, endDate }
+        });
+
+    });
 });
